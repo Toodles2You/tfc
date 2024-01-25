b3cb8152e9a89dcbf5388ac1a892c8db8d9c1ffd
Justin <95296300+Toodles2You@users.noreply.github.com>
2024-01-24 21:04:22 -0800

    Prevent weapon dropping if gamerules forbid it

--- src/server/entities/player.cpp
+++ src/server/entities/player.cpp
@@ -1594,6 +1594,11 @@ void CBasePlayer::DropPlayerWeapon(char* pszWeaponName)
 		return;
 	}
 
+	if (g_pGameRules->DeadPlayerWeapons(this) == GR_PLR_DROP_GUN_NO)
+	{
+		return;
+	}
+
 	CBasePlayerWeapon *pWeapon = nullptr;
 
 	if (pszWeaponName != nullptr && pszWeaponName[0] != '\0')
