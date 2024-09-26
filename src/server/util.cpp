4c494c997b837fa64be33398e9b1936123cfa9a4
Justin <95296300+Toodles2You@users.noreply.github.com>
2024-09-26 16:59:22 -0700

    Disable projectile lag compensation
    It's causing crashes & I don't care enough to look into it right now.

--- src/server/util.cpp
+++ src/server/util.cpp
@@ -1216,8 +1216,9 @@ bool util::IsDeathmatch()
 	return util::GetGameMode() > kGamemodeCooperative;
 }
 
-void util::LagCompensation(CBaseEntity* entity, const int& ping)
+void util::LagCompensation(CBaseEntity* entity, const int ping)
 {
+#if 0
 	if (AllowLagCompensation() == 0 || ping <= 5)
 	{
 		return;
@@ -1281,4 +1282,5 @@ void util::LagCompensation(CBaseEntity* entity, const int& ping)
 			entity->SetOrigin(trace.vecEndPos);
 		}
 	}
+#endif
 }
